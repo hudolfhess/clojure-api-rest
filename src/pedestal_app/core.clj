ceb74ecf2dab977569cc939f8b034cd2fe81f53b
Hudolf Hess <hudolf@gmail.com>
2016-07-27 23:33:48 -0300

    Initial commit with pedestal-app

---
+++ src/pedestal_app/core.clj
@@ -0,0 +1,1 @@
+(ns pedestal-app.core)
---
+++ test/pedestal_app/test/core.clj
@@ -0,0 +1,6 @@
+(ns pedestal-app.test.core
+  (:use [pedestal-app.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
